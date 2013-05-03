1519ef33735f52c31e136d5301ae5c0b9c7b89d7
Sandy <santhai.phan@gmail.com>
2013-05-03 11:40:22 +1000

    Created interfaces for 3 methods to be used in generating a puzzle.

--- Project/src/Puzzle.java
+++ Project/src/Puzzle.java
@@ -6,6 +6,55 @@
  *
  */
 public class Puzzle {
+	
+	/**
+	 * Create a new Sudoku puzzle with the
+	 * specified difficulty level.
+	 * @param difficultyLevel The difficulty level
+	 * partitioned into Easy, Medium and Hard.
+	 */
+	public Puzzle(int difficultyLevel) {
+		this.difficultyLevel = difficultyLevel;
+		initialisePuzzle();
+		generatePuzzle();
+	}
+	
+	/**
+	 * Generate the whole puzzle with appropriate
+	 * cells removed according to the difficulty
+	 * level specified.
+	 */
+	private void generatePuzzle() {
+		removeCells();
+	}
+	
+	/**
+	 * Initialize the puzzle by setting
+	 * the values of the rows, columns
+	 * and 3x3 box a square should be in.
+	 */
+	private void initialisePuzzle() {
+		
+	}
+	
+	/**
+	 * Remove the number of cells according
+	 * to the difficulty level set from the
+	 * constructor.
+	 */
+	private void removeCells() {
+		
+	}
 
-	private Square puzzle[][] = new Square[9][9];
+	private Square puzzle[][] = new Square[ROW_NUMBER][COLUMN_NUMBER];
+	private int difficultyLevel;
+	
+	// levels of difficulty (not sure where to place them yet).
+	// Should this be placed in the main function or should this be
+	// placed in any classes that have a difficulty defined.
+	private static final int EASY = 0;
+	private static final int MEDIUM = 1;
+	private static final int HARD = 2;
+	private static final int ROW_NUMBER = 9;
+	private static final int COLUMN_NUMBER = 9;
 }
