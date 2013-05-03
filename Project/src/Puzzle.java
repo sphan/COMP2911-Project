cccda0bd92a8670f6a9c75bd148af4821a6e6370
Sandy <santhai.phan@gmail.com>
2013-05-03 11:11:33 +1000

    Declared a 2D array of square instead of 2D array of integers.

---
+++ Project/src/Puzzle.java
@@ -0,0 +1,11 @@
+
+/**
+ * A class that generates the Sudoku puzzle
+ * according the level specified.
+ * @author tsph001 - Sandy Phan
+ *
+ */
+public class Puzzle {
+
+	private Square puzzle[][] = new Square[9][9];
+}
