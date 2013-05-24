b38e0b87582d30e7021b193885f29588e795bcb7
Sandy <santhai.phan@gmail.com>
2013-05-24 11:36:11 +1000

    Added other tests to generate puzzles in 3 difficulty levels. Still not sure if all puzzles have 1 single solution or not.

--- Project/src/PuzzleTester.java
+++ Project/src/PuzzleTester.java
@@ -2,7 +2,15 @@
 public class PuzzleTester {
 
 	public static void main(String args[]) {
+		System.out.println("Easy puzzle:");
 		Puzzle.createPuzzle(0);
+		System.out.println();
+		System.out.println("Medium puzzle:");
+		Puzzle.createPuzzle(1);
+		System.out.println();
+		System.out.println("Hard puzzle:");
+		Puzzle.createPuzzle(2);
+		System.out.println();
 //		p.printPuzzle();
 	}
 }
