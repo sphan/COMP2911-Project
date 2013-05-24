6630459803f669c985bd637d797380291f5f1ba1
Richard <Richard@richard3476@gmail.com>
2013-05-24 19:52:02 +1000

    Should work, just needs polishing

--- Project/src/PuzzleTester.java
+++ Project/src/PuzzleTester.java
@@ -2,6 +2,9 @@
 public class PuzzleTester {
 
 	public static void main(String args[]) {
+		int[][] Sudoku1 = new int[9][9]; 
+		HintSystem h = new HintSystem();
+		h.Hint(Sudoku1);
 		Puzzle p = new Puzzle(0);
 //		p.printPuzzle();
 	}
--- Project/src/Unit.java
+++ Project/src/Unit.java
@@ -28,19 +28,20 @@ public class Unit {
 	public ArrayList<Unit> getPeers(Unit a) {
 		ArrayList<Unit> toReturn = new ArrayList<Unit>();
 		for (int i = 0; i < 9; i++) {
-			if (i <= a.getX()) {
+			if (i != a.getX()) {
 				toReturn.add(new Unit(i, a.getY()));
 			}
 		}
 		for (int i = 0; i < 9; i++) {
-			if (i <= a.getY()) {
+			if (i != a.getY()) {
 				toReturn.add(new Unit(i, a.getX()));
 			}
 		}
-		int first = a.getX()%3;
-		int second = a.getY()%3;
-		for (int i = first*3 - 3; i < first*3; i++) {
-			for (int j = second*3 - 3; j < second*3; j++) {
+		int first = a.getX()/3;
+		int second = a.getY()/3;
+		//System.out.println("unit x" + first + " y" + second);
+		for (int i = first*3; i < first*3 + 3; i++) {
+			for (int j = second*3; j < second*3 + 3; j++) {
 				Unit buffer = new Unit(i, j);
 				if (toReturn.contains(buffer) == false && buffer.equals(a) == false) {
 					toReturn.add(buffer);
@@ -54,8 +55,8 @@ public class Unit {
 		ArrayList<Unit> toReturn = new ArrayList<Unit>();
 		int first = a.getX()%3;
 		int second = a.getY()%3;
-		for (int i = first*3 - 3; i < first*3; i++) {
-			for (int j = second*3 - 3; j < second*3; j++) {
+		for (int i = first*3; i < first*3 + 3; i++) {
+			for (int j = second*3; j < second*3 + 3; j++) {
 				Unit buffer = new Unit(i, j);
 				if (buffer.equals(a) == false) {
 					toReturn.add(buffer);
