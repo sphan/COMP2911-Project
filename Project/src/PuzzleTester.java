7db4f3a9e50f1bec03f515acea0040549292d708
sphan <santhai.phan@gmail.com>
2013-05-25 17:01:54 +1000

    Removed conflict comments

--- Project/src/PuzzleTester.java
+++ Project/src/PuzzleTester.java
@@ -1,8 +1,7 @@
-
 public class PuzzleTester {
 
 	public static void main(String args[]) {
-<<<<<<< HEAD
+
 		System.out.println("Easy puzzle:");
 		Puzzle.createPuzzle(Puzzle.EASY);
 		System.out.println();
@@ -12,12 +11,12 @@ public class PuzzleTester {
 		System.out.println("Hard puzzle:");
 		Puzzle.createPuzzle(Puzzle.HARD);
 		System.out.println();
-=======
+
 		int[][] Sudoku1 = new int[9][9]; 
 		HintSystem h = new HintSystem();
 		h.Hint(Sudoku1);
 		Puzzle p = new Puzzle(0);
->>>>>>> 6630459803f669c985bd637d797380291f5f1ba1
+
 //		p.printPuzzle();
 	}
 }
